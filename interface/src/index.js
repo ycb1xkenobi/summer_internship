9b82f8050d0863ef35b98fa819fe2202b6f6de28
Denis Sukhin <88856699+xFlodz@users.noreply.github.com>
2022-07-18 18:43:14 +0300

    Delete interface directory

--- interface/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import * as ReactDOMClient from "react-dom/client";
-import App from "./App";
-import "./css/style.css"
-
-
-const app = ReactDOMClient.createRoot(document.getElementById("app"))
-
-app.render(<App/>)
